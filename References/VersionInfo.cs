0c306b09a5d43c08e12eb7ca7b15ac9dff27a839
Chris Doty (Civic) <dotyc@civicinc.com>
2013-09-12 20:48:43 -0500

    update version

--- References/VersionInfo.cs
+++ References/VersionInfo.cs
@@ -2,6 +2,6 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
 [assembly: AssemblyCompany("Civic Engineering & IT")]
 [assembly: AssemblyCopyright("Copyright © 2012 Civic Engineering & IT")]
