1996c0c4d889d3afae1c605454397e78100606cb
Chris Doty (Civic) <dotyc@civicinc.com>
2013-09-20 08:30:23 -0500

    fix schema for usp_processfilter sp

--- References/VersionInfo.cs
+++ References/VersionInfo.cs
@@ -2,6 +2,6 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
 [assembly: AssemblyCompany("Civic Engineering & IT")]
 [assembly: AssemblyCopyright("Copyright © 2012 Civic Engineering & IT")]
