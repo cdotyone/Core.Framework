3889e683a620dc5f08b66d5701404af997cffd4e
unknown <dotyc@PCLDOTYC01.CivicInc.local>
2013-12-03 23:53:03 -0600

    nuget

--- Civic.T4.WebApi/nuget/pack.bat
+++ Civic.T4.WebApi/nuget/pack.bat
@@ -4,6 +4,8 @@ del  %2*.nupkg
 SET PATH=%4.nuget;%PATH%
 AssemblyVersionFinder.exe -s -d %4\references > tmpFile
 set /p version=<tmpFile
+set nugetconfig=%4.nuget\nuget.config
+echo %nugetconfig%
 IF %3==Nuget (
 	FOR %%X in ("%1nuget\*.nuspec") DO (
 		ECHO nuget.exe pack %%X -Version %version%
@@ -11,7 +13,7 @@ IF %3==Nuget (
 	)
 
 	FOR %%X in ("%2*.nupkg") DO (
-		ECHO nuget.exe push %%X
-		nuget.exe push %%X
+		ECHO nuget.exe push %%X -Config %nugetconfig%
+		nuget.exe push %%X -Config %nugetconfig%
 	)
 )
