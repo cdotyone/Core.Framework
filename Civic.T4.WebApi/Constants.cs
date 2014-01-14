f4bd7be4a7a243c7b86445840b516cb6505fec42
Chris Doty <dotyc@civicinc.com>
2014-01-13 23:28:33 -0600

    add maxrow configuration

--- Civic.T4.WebApi/Constants.cs
+++ Civic.T4.WebApi/Constants.cs
@@ -6,6 +6,7 @@
 		public const string CONFIG_SECTION = "t4WebApi";
    	    public const string CONFIG_ASSEMBLY = "assembly";
    	    public const string CONFIG_ASSEMBLIES = "assemblies";
+        public const string CONFIG_MAXROW = "maxRowOverrides";
 
 	}
 }
