6bf7b92a3ad3883e87bf37d17bbec834fcceae9d
Chris Doty <dotyc@civicinc.com>
2014-07-16 15:12:29 -0500

    update webapi packages to reflect purpose

--- References/VersionInfo.cs
+++ References/VersionInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
 [assembly: AssemblyCompany("Civic Engineering & IT")]
 [assembly: AssemblyCopyright("Copyright © 2013 Civic Engineering & IT")]
