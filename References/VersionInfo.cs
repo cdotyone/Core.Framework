1a817a6c8ea96f8fe128611692aa412f552a76c4
Chris Doty <dotyc@civicinc.com>
2014-08-07 09:14:08 -0500

    bump version

--- References/VersionInfo.cs
+++ References/VersionInfo.cs
@@ -2,6 +2,6 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("2.4.0.0")]
-[assembly: AssemblyFileVersion("2.4.0.0")]
+[assembly: AssemblyFileVersion("2.5.0.0")]
 [assembly: AssemblyCompany("Civic Engineering & IT")]
 [assembly: AssemblyCopyright("Copyright © 2013 Civic Engineering & IT")]
