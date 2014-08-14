53fc48af732c28ad2f374d01221565e465485626
Chris Doty <dotyc@civicinc.com>
2014-08-14 13:14:17 -0500

    cleanup for previous bug

--- Civic.T4.WebApi/Constants.cs
+++ Civic.T4.WebApi/Constants.cs
@@ -4,7 +4,6 @@
 	{
 
    	    public const string CONFIG_SECTION = "civicT4";
-        public const string CONFIG_MAXROW = "maxRowOverrides";
 
         public const string CONFIG_FORCEUPPER = "forceUpperCase";
         public const string CONFIG_MAX = "max";
