bb12ed9c6c9e8b84798f3ee8beafde2535c9f25a
Chris Doty <dotyc@civicinc.com>
2014-09-28 23:34:46 -0500

    update nuget packages

--- References/VersionInfo.cs
+++ References/VersionInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyVersion("2.7.0.0")]
-[assembly: AssemblyFileVersion("2.7.0.0")]
+[assembly: AssemblyFileVersion("2.8.0.0")]
 [assembly: AssemblyCompany("Civic Engineering & IT")]
 [assembly: AssemblyCopyright("Copyright © 2013 Civic Engineering & IT")]
