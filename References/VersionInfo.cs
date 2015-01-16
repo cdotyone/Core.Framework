e7133fdac495498a2b65617a19dc577d440a2145
Chris Doty <dotyc@civicinc.com>
2015-01-16 13:54:18 -0600

    update copyrights

--- References/VersionInfo.cs
+++ References/VersionInfo.cs
@@ -3,4 +3,4 @@
 [assembly: AssemblyVersion("2.9.0.0")]
 [assembly: AssemblyFileVersion("2.9.0.0")]
 [assembly: AssemblyCompany("Civic Engineering & IT")]
-[assembly: AssemblyCopyright("Copyright © 2013 Civic Engineering & IT")]
+[assembly: AssemblyCopyright("Copyright © 2015 Civic Engineering & IT")]
