f9ebeb105e2a25f09150df1e96cd924fdc1db728
Chris Doty <dotyc@civicinc.com>
2015-11-02 16:00:33 -0600

    add configuration element to service

--- Civic.T4.WebApi/IEntityService.cs
+++ Civic.T4.WebApi/IEntityService.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Civic.Core.Configuration;
 
 namespace Civic.T4.WebApi
 {
     public interface IEntityService
     {
+        INamedElement Configuration { get; set; }
+
         string ModuleName { get; }
 
         List<string> EntitiesProvided { get; }
