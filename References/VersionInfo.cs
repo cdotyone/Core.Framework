a60563bbc93e085bdb89a61cc346db6bcf7f240f
Chris Doty <dotyc@civicinc.com>
2016-08-22 09:45:42 -0500

    remove References to add submodule

--- References/VersionInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyVersion("2.16.0.0")]
-[assembly: AssemblyFileVersion("2.16.0.0")]
-[assembly: AssemblyCompany("Civic Engineering & IT")]
-[assembly: AssemblyCopyright("Copyright © 2015 Civic Engineering & IT")]
