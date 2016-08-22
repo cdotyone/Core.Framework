3f6585de75fcbe38978bab39a8c4aa22335fea34
dotyc <dotyc@civicinc.com>
2016-08-22 09:49:48 -0500

    add References

---
+++ Civic.Framework.WebApi/OData/ExpressionTypes.cs
@@ -0,0 +1,10 @@
+﻿namespace Civic.Framework.WebApi.OData
+{
+    public enum ExpressionTypes
+    {
+        Criteria, 
+        AndCriteria, 
+        OrCriteria, 
+        Function
+    }
+}
---
+++ Civic.Framework.WebApi/OData/IExpression.cs
@@ -0,0 +1,10 @@
+﻿namespace Civic.Framework.WebApi.OData
+{
+    public interface IExpression
+    {
+        ExpressionTypes Type { get; }
+
+        IExpression NestedExpression { get; set; }
+    }
+
+}
