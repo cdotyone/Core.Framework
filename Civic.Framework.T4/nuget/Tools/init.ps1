b9619313bec5a7ea77e04c0dabe852c0ea41b13a
Chris Doty <dotyc@civicinc.com>
2018-01-13 10:18:28 -0600

    ...

--- Civic.Framework.T4/nuget/Tools/init.ps1
+++ Civic.Framework.T4/nuget/Tools/init.ps1
@@ -1,14 +1,14 @@
 ﻿#param($rootPath, $toolsPath, $package, $project)
 
-Write-Output Root Path $rootPath
-Write-Output ToolsPath $toolsPath
-Write-Output Package $package
+Write-Output "Root Path $rootPath"
+Write-Output "ToolsPath $toolsPath"
+Write-Output "Package $package"
 
 $regex = new-object System.Text.RegularExpressions.Regex ('(.*\\).*', [System.Text.RegularExpressions.RegexOptions]::MultiLine)
 $base = $regex.split($toolsPath)[1]
 
 $rootPath += '0.0.0.0'
 
-Write-Output Copying Templates From $rootPath To $base
+Write-Output "Copying Templates From $rootPath To $base"
 
 Copy-Item $rootPath $base -recurse -force
