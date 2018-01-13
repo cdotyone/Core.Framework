36d135d33f075b70ad4212d5c09348d85ea25221
Chris Doty <dotyc@civicinc.com>
2018-01-13 10:13:25 -0600

    ...

--- Civic.Framework.T4/nuget/Tools/init.ps1
+++ Civic.Framework.T4/nuget/Tools/init.ps1
@@ -1,7 +1,11 @@
 ﻿#param($rootPath, $toolsPath, $package, $project)
 
+Write-Output Root Path $rootPath
+Write-Output ToolsPath $toolsPath
+Write-Output Package $package
+
 $regex = new-object System.Text.RegularExpressions.Regex ('(.*\\).*', [System.Text.RegularExpressions.RegexOptions]::MultiLine)
-$base = $regex.split($rootPath)[1]
+$base = $regex.split($toolsPath)[1]
 
 $rootPath += '0.0.0.0'
 
