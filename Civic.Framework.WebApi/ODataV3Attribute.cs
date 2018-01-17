d14d599913eb4f8bbd6275f36abeeaa760f1abdb
Chris Doty <dotyc@civicinc.com>
2018-01-17 01:10:31 -0600

    make owin ODATAv3 work

--- Civic.Framework.WebApi/ODataV3Attribute.cs
+++ Civic.Framework.WebApi/ODataV3Attribute.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Web;
 using System.Web.Http.Controllers;
 using System.Web.Http.Filters;
+using Civic.Core.Logging;
 
 namespace Civic.Framework.WebApi
 {
@@ -13,44 +15,88 @@ namespace Civic.Framework.WebApi
         {
             if (actionContext == null)
             {
-                throw new ArgumentNullException("actionContext");
+                throw new ArgumentNullException(nameof(actionContext));
             }
             var request = actionContext.Request;
-            if (request == null)
-            {
-                //throw Error.Argument("actionExecutedContext", SRResources.ActionExecutedContextMustHaveRequest, new object[0]);
-            }
-            else
+            if (request != null)
             {
                 var raw = new ODataV3QueryOptions();
 
-                var httpContext = request.Properties["MS_HttpContext"] as HttpContextBase;
-                if (httpContext != null)
+                if (request.Properties.ContainsKey("MS_QueryNameValuePairs"))
                 {
-                    var requestContext = httpContext.Request;
-
-                    if (!string.IsNullOrEmpty(requestContext["$top"]))
+                    try
                     {
-                        int top;
-                        int.TryParse(requestContext["$top"], NumberStyles.Integer, null, out top);
-                        raw.Top = top;
+                        var query = (KeyValuePair<string, string>[])request.Properties["MS_QueryNameValuePairs"];
+
+                        Dictionary<string, string> requestContext = new Dictionary<string, string>();
+                        foreach (var pair in query)
+                        {
+                            requestContext[pair.Key.ToLowerInvariant()] = pair.Value;
+                        }
+
+                        if (requestContext.ContainsKey("$top"))
+                        {
+                            int.TryParse(requestContext["$top"], NumberStyles.Integer, null, out var top);
+                            raw.Top = top;
+                        }
+
+                        if (requestContext.ContainsKey("$skip"))
+                        {
+                            int.TryParse(requestContext["$skip"], NumberStyles.Integer, null, out var skip);
+                            raw.Skip = skip;
+                        }
+
+                        if (requestContext.ContainsKey("$inlinecount"))
+                            raw.InlineCount = string.Compare(requestContext["$inlinecount"], "allpages", StringComparison.InvariantCultureIgnoreCase) == 0;
+
+                        if (requestContext.ContainsKey("$orderby"))
+                            raw.OrderBy = requestContext["$orderby"];
+                        if (requestContext.ContainsKey("$filter"))
+                            raw.Filter = requestContext["$filter"];
+                        if (requestContext.ContainsKey("$expand"))
+                            raw.Expand = requestContext["$expand"];
+                        if (requestContext.ContainsKey("$format"))
+                            raw.Format = requestContext["$format"];
+
+                        actionContext.ControllerContext.RouteData.Values.Add("options", raw);
                     }
-                    if (!string.IsNullOrEmpty(requestContext["$skip"]))
+                    catch (Exception ex)
                     {
-                        int skip;
-                        int.TryParse(requestContext["$skip"], NumberStyles.Integer, null, out skip);
-                        raw.Skip = skip;
+                        Logger.HandleException(LoggingBoundaries.ServiceBoundary, ex);
                     }
-
-                    raw.InlineCount = string.Compare(requestContext["$inlinecount"], "allpages", StringComparison.InvariantCultureIgnoreCase) == 0;
-
-                    raw.OrderBy = requestContext["$orderby"];
-                    raw.Filter = requestContext["$filter"];
-                    raw.Expand = requestContext["$expand"];
-                    raw.Format = requestContext["$format"];
-
-                    actionContext.ControllerContext.RouteData.Values.Add("options", raw);
                 }
+
+                else 
+
+                if (request.Properties.ContainsKey("MS_HttpContext"))
+                {
+                    if (request.Properties["MS_HttpContext"] is HttpContextBase httpContext)
+                    {
+                        var requestContext = httpContext.Request;
+
+                        if (!string.IsNullOrEmpty(requestContext["$top"]))
+                        {
+                            int.TryParse(requestContext["$top"], NumberStyles.Integer, null, out var top);
+                            raw.Top = top;
+                        }
+
+                        if (!string.IsNullOrEmpty(requestContext["$skip"]))
+                        {
+                            int.TryParse(requestContext["$skip"], NumberStyles.Integer, null, out int skip);
+                            raw.Skip = skip;
+                        }
+
+                        raw.InlineCount = string.Compare(requestContext["$inlinecount"], "allpages",
+                                              StringComparison.InvariantCultureIgnoreCase) == 0;
+
+                        raw.OrderBy = requestContext["$orderby"];
+                        raw.Filter = requestContext["$filter"];
+                        raw.Expand = requestContext["$expand"];
+                        raw.Format = requestContext["$format"];
+
+                        actionContext.ControllerContext.RouteData.Values.Add("options", raw);
+                    }
+                }                 
             }
 
             base.OnActionExecuting(actionContext);
