cecb93d093dc28350b1a790a7ce25a293bb602e2
Chris Doty <dotyc@civicinc.com>
2018-09-13 13:29:21 -0500

    feature - moving away from HttpContext

--- Civic.Framework.WebApi/AuthorizationHelper.cs
+++ Civic.Framework.WebApi/AuthorizationHelper.cs
@@ -16,32 +16,20 @@ namespace Civic.Framework.WebApi
             if (context == null) return true; // NOT a web users, security to be handled by application
 
             var claimsPrincipal = context.User as ClaimsPrincipal;
-            if (claimsPrincipal != null)
-            {
-                return
-                    claimsPrincipal.IsInRole(module.ToUpperInvariant() + "." + entityname.ToUpperInvariant() + "_M") ||
-                    claimsPrincipal.IsInRole(module.ToUpperInvariant() + "." + entityname.ToUpperInvariant() + "_F");
-            }
 
-            return false;
+            return CanModify(claimsPrincipal, module, entityname);
         }
 
         public static bool CanAdd(string module, string entityname)
         {
-            if (T4Config.GetCanAdd(module, entityname)) return true;
+            if (T4Config.GetCanModify(module, entityname)) return true;
 
             var context = HttpContext.Current;
             if (context == null) return true; // NOT a web users, security to be handled by application
 
             var claimsPrincipal = context.User as ClaimsPrincipal;
-            if (claimsPrincipal != null)
-            {
-                return
-                    claimsPrincipal.IsInRole(module.ToUpperInvariant() + "." + entityname.ToUpperInvariant() + "_A") ||
-                    claimsPrincipal.IsInRole(module.ToUpperInvariant() + "." + entityname.ToUpperInvariant() + "_F");
-            }
 
-            return false;
+            return CanAdd(claimsPrincipal, module, entityname);
         }
 
         [Obsolete("Please use CanRemove Instead")]
@@ -52,12 +40,70 @@ namespace Civic.Framework.WebApi
 
         public static bool CanRemove(string module, string entityname)
         {
-            if (T4Config.GetCanRemove(module, entityname)) return true;
+            if (T4Config.GetCanModify(module, entityname)) return true;
 
             var context = HttpContext.Current;
             if (context == null) return true; // NOT a web users, security to be handled by application
 
             var claimsPrincipal = context.User as ClaimsPrincipal;
+
+            return CanRemove(claimsPrincipal, module, entityname);
+        }
+
+        public static bool CanView(string module, string entityname)
+        {
+            if (T4Config.GetCanModify(module, entityname)) return true;
+
+            var context = HttpContext.Current;
+            if (context == null) return true; // NOT a web users, security to be handled by application
+
+            var claimsPrincipal = context.User as ClaimsPrincipal;
+
+            return CanView(claimsPrincipal, module, entityname);
+        }
+
+        public static bool HasPermission(string module, string entityname, string permission, bool exact = false)
+        {
+            if (T4Config.GetCanModify(module, entityname)) return true;
+
+            var context = HttpContext.Current;
+            if (context == null) return true; // NOT a web users, security to be handled by application
+
+            var claimsPrincipal = context.User as ClaimsPrincipal;
+
+            return HasPermission(claimsPrincipal, module, entityname, permission, exact);
+        }
+
+
+        public static bool CanModify(ClaimsPrincipal claimsPrincipal, string module, string entityname)
+        {
+            if (T4Config.GetCanModify(module, entityname)) return true;
+            if (claimsPrincipal != null)
+            {
+                return
+                    claimsPrincipal.IsInRole(module.ToUpperInvariant() + "." + entityname.ToUpperInvariant() + "_M") ||
+                    claimsPrincipal.IsInRole(module.ToUpperInvariant() + "." + entityname.ToUpperInvariant() + "_F");
+            }
+
+            return false;
+        }
+
+        public static bool CanAdd(ClaimsPrincipal claimsPrincipal, string module, string entityname)
+        {
+            if (T4Config.GetCanAdd(module, entityname)) return true;
+            if (claimsPrincipal != null)
+            {
+                return
+                    claimsPrincipal.IsInRole(module.ToUpperInvariant() + "." + entityname.ToUpperInvariant() + "_A") ||
+                    claimsPrincipal.IsInRole(module.ToUpperInvariant() + "." + entityname.ToUpperInvariant() + "_F");
+            }
+
+            return false;
+        }
+
+        public static bool CanRemove(ClaimsPrincipal claimsPrincipal, string module, string entityname)
+        {
+            if (T4Config.GetCanRemove(module, entityname)) return true;
             if (claimsPrincipal != null)
             {
                 return
@@ -68,14 +114,10 @@ namespace Civic.Framework.WebApi
             return false;
         }
 
-        public static bool CanView(string module, string entityname)
+        public static bool CanView(ClaimsPrincipal claimsPrincipal, string module, string entityname)
         {
             if (T4Config.GetCanView(module, entityname)) return true;
 
-            var context = HttpContext.Current;
-            if (context == null) return true; // NOT a web users, security to be handled by application
-
-            var claimsPrincipal = context.User as ClaimsPrincipal;
             if (claimsPrincipal != null)
             {
                 return
@@ -86,12 +128,8 @@ namespace Civic.Framework.WebApi
             return false;
         }
 
-        public static bool HasPermission(string module, string entityname, string permission, bool exact = false)
+        public static bool HasPermission(ClaimsPrincipal claimsPrincipal, string module, string entityname, string permission, bool exact = false)
         {
-            var context = HttpContext.Current;
-            if (context == null) return true; // NOT a web users, security to be handled by application
-
-            var claimsPrincipal = context.User as ClaimsPrincipal;
             if (claimsPrincipal != null)
             {
                 return
