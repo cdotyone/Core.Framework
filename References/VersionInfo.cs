db7a5eb9fa02dd0631888cc84cf71451534934b5
Chris Doty <dotyc@civicinc.com>
2020-07-24 00:59:50 -0500

    chore: more cleanup

--- References/VersionInfo.cs
+++ References/VersionInfo.cs
@@ -3,4 +3,4 @@ using System.Reflection;
 [assembly: AssemblyVersion("3.0.0.0")]
 [assembly: AssemblyFileVersion("3.1.20.5847")]
 [assembly: AssemblyCompany("Chris Doty")]
-[assembly: AssemblyCopyright("Copyright © 2019 Chris Doty")]
+[assembly: AssemblyCopyright("Copyright © 2020 Chris Doty")]
